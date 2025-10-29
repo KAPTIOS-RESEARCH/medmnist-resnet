f85f7e572902b5f4c7acd9b76ff78faf6a44d56e
bniepce <brad.niepce@gmail.com>
2025-10-29 19:58:03 +0100

    Add n_input

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -3,11 +3,11 @@ from torchvision import models
 
 
 class ResNet18(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, n_input, num_classes):
         super().__init__()
         self.model = models.resnet18(weights=None)
         self.model.conv1 = nn.Conv2d(
-            1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+            n_input, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
     def forward(self, x):
